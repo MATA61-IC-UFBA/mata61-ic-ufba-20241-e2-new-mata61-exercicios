46d7df9433991d6c98dca89e69a2abb2ae5b4c7b
Iure Guimaraes <iurevguimaraes@gmail.com>
2024-08-12 22:39:48 -0300

    E2-new-iure-guimaraes

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,22 @@
 /* main.c */
 
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex();
+extern char *yytext;
+
+int main() {
+	token_t token = -1;
+
+	while(token != EOL) {
+		token=yylex();												// call yylex()
+		if(token == ERROR)
+			printf("erro léxico: %s\n", yytext);
+		else
+			if(token == NUMBER)
+				printf("constante decimal: %s\n", yytext);
+			else
+				printf("código do token: %d\n", token);
+	}
+}
